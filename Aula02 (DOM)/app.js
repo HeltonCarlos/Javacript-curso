fd44a8d52d2554bb31ab4039c26f34e17a98f7eb
HeltonCarlos <heltonbritopa@gmail.com>
2020-05-20 17:40:41 -0300

    dom README
    DOM

---
+++ Aula02 (DOM)/app.js
@@ -0,0 +1,19 @@
+var inputElement = document.getElementById('nome');
+console.log(inputElement);
+
+var inputElement2 = document.getElementsByClassName('tarefa');
+console.log(inputElement2);
+
+var mudanca=document.querySelector('input')
+console.log(mudanca);
+
+function btn1(){
+    alert('Helton 01')
+}
+function btn2(){
+    alert('Helton 02')
+}
+function btn3(){
+    alert('Helton 03')
+}
+
