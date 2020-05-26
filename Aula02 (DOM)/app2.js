7975f107dc244ed1d831cbf773c4e46f8149933f
HeltonCarlos <heltonbritopa@gmail.com>
2020-05-26 14:27:25 -0300

    dom2 README
    dom2

--- Aula02 (DOM)/app2.js
+++ Aula02 (DOM)/app2.js
@@ -15,3 +15,19 @@ var elemento=document.querySelector('button.btn');
 elemento.onclick= function () {
     alert ("geral");
   }
+  //adecionar o link
+
+  var linkjow = document.createElement('a')
+  linkjow.setAttribute('href','https://www.globo.com/')
+
+  var text= document.createTextNode('Globo')
+  linkjow.appendChild(text)
+
+  var container=document.querySelector('#alocar')
+  container.appendChild(linkjow);
+
+//stylo
+
+var modelo= document.querySelector('.modelar')
+modelo.style.color ='red';
+
