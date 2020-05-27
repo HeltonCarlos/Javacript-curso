5ff1d3ca6336e52d14e9f3f23e51eb99fe41add8
HeltonCarlos <heltonbritopa@gmail.com>
2020-05-27 16:34:53 -0300

    todo README
    TODO

--- Aula03(todos)/app.js
+++ Aula03(todos)/app.js
@@ -1,1 +1,9 @@
-alert('vamos estudar')
+var lista = document.querySelector('#app ul');
+var espaco = document.querySelector('#app input')
+var btn= document.querySelector('#app button')
+
+var todos = [
+    'Tomar café'
+    'Almoço'
+   ' Jantar'
+];
