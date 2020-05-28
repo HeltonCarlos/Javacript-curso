3595a1c944f38dc955f1a1fe1a7afc04332a4485
HeltonCarlos <heltonbritopa@gmail.com>
2020-05-27 22:33:34 -0300

    todo-create README
    CREATE

--- Aula03(todos)/app.js
+++ Aula03(todos)/app.js
@@ -3,7 +3,30 @@ var espaco = document.querySelector('#app input')
 var btn= document.querySelector('#app button')
 
 var todos = [
-    'Tomar café'
-    'Almoço'
+    'Tomar café',
+    'Almoço',
    ' Jantar'
 ];
+
+function renderTodos(){
+
+    lista.innerHTML='';
+    for(todo of todos){
+       
+
+        var todoEle =document.createElement('li');
+        var todoText= document.createTextNode(todo);
+        todoEle.appendChild(todoText);
+        lista.appendChild(todoEle)
+    }
+}renderTodos();
+
+function addTodo(){
+    var todoText = espaco.value;
+
+    todos.push(todoText);
+    espaco.value='';
+    renderTodos();
+}
+
+btn.onclick = addTodo;
