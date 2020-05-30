995845d1805285261643a8c71740e6865a1d0d90
HeltonCarlos <heltonbritopa@gmail.com>
2020-05-30 20:37:01 -0300

    finalizando JAVASCRIPT
    Etapa 1 javascript ES6

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*função*/
+/*classe*/
 class Matematica{
     static soma(a,b){
         return a+b;
@@ -48,3 +48,49 @@ const{nome,idade,endereco:{cidade}}=usuario
 console.log(nome);
 console.log(idade);
 console.log(cidade);
+//resto
+
+let alunos={
+    sobrenome:'BRITO',
+    idade:29,
+    cidade:'Maceio'
+};
+let {  sobrenome, ...resto} = alunos;
+console.log(nome);
+console.log(resto);
+
+//SPREAD
+
+const arr01=[1,2,3];
+const arr02=[4,5,6];
+
+const arr3=[...arr01,...arr02];
+console.log(arr3);
+//segundo modelo spread
+const carros={
+    fiat:'uno',
+    ford:'fordka',
+    honda:'honda',
+}
+
+const carros1={...carros, fiat:'siena'}
+console.log(carros1);
+
+
+//template literals
+const cachorro='pluto';
+const tempodevida=12;
+console.log(`Meu cachorro é ${cachorro} e tenho ${tempodevida} anos !`);
+
+//Object Short Syntax
+const moto='xtz125';
+const modelo=2016;
+
+const motor={
+    moto,
+    modelo,
+    cor:'azul'
+};
+console.log(motor);
+
+
