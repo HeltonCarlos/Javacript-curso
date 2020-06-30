46ac4857a1d44773e500d5f6d558790abf4261be
HeltonCarlos <heltonbritopa@gmail.com>
2020-06-29 22:18:56 -0300

    modelo04 README
    Modulo04

---
+++ modelo04/app.js
@@ -0,0 +1,11 @@
+
+var hora = 22;
+
+if(hora>=0 && hora<=11){
+    console.log('bom dia')
+    
+}else if(hora>=12 && hora<=17){
+    console.log('Boa tarde')
+}else{
+    console.log('Boa noite')
+}
