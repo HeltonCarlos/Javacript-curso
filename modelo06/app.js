1d2842132dccfd9a65fe92c3b07b8d2f9fa7808c
HeltonCarlos <heltonbritopa@gmail.com>
2020-07-06 21:44:56 -0300

    valor README
    valor

--- modelo06/app.js
+++ modelo06/app.js
@@ -1,6 +1,10 @@
 function enviar(){
     var num=document.querySelector('#numero')
     var vl=document.getElementById('valor')
+    var total= Number(num.value)
 
+    if(total.value==null){
+        vl.innerHTML+=`Digite um numero`
+    }
     
 }
