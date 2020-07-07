731fe0a4a87c9d0f35827adc68d2f12879b045ab
HeltonCarlos <heltonbritopa@gmail.com>
2020-07-07 19:44:45 -0300

    tabuada README
    Tabuada

--- modelo06/app.js
+++ modelo06/app.js
@@ -3,8 +3,19 @@ function enviar(){
     var vl=document.getElementById('valor')
     var total= Number(num.value)
 
-    if(total.value==null){
+    if(total==''){
         vl.innerHTML+=`Digite um numero`
+    }else {
+        vl.innerHTML+=`Divisão de ${total} é de:
+        <br>${total}x2=${total*2}
+        <br>${total}x3=${total*3}
+        <br>${total}x4=${total*4}
+        <br>${total}x5=${total*5}
+        <br>${total}x6=${total*6}
+        <br>${total}x7=${total*7}
+        <br>${total}x8=${total*8}
+        <br>${total}x9=${total*9}
+        <br>${total}x10=${total*10}`
     }
     
 }
