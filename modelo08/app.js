eb6e673e1ac312bc470c4f4813529765bbf9450b
HeltonCarlos <heltonbritopa@gmail.com>
2020-07-27 19:58:19 -0300

    alert README
    Alerta de erro

---
+++ modelo08/app.js
@@ -0,0 +1,12 @@
+
+function enviar(){
+    var mail =document.querySelector('#email').value;
+    var pass=document.querySelector('#senha').value;
+    var pagina=document.getElementById('pag');
+
+    if(!mail && !pass){
+       alert('Digite os campos...');
+    }else{
+        
+    }
+}
