6686c32eb4875cef9a792a95034bf17aaf46049a
HeltonCarlos <heltonbritopa@gmail.com>
2020-07-27 20:27:36 -0300

    modulo09 README
    Modulo09

---
+++ modelo09/app.js
@@ -0,0 +1,10 @@
+var p = document.createElement('p');
+p.innerHTML=`Usando elemento p`;
+
+var img=document.createElement('img');
+
+img.src="imagem.jpg"
+
+
+var pag = document.getElementById('pag').appendChild(p)
+var pag = document.getElementById('pag').appendChild(img)
